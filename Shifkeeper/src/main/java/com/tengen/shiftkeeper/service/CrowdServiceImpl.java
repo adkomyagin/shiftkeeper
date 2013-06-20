2cc74febf6b089f7752d747eb5a87ef3e8918570
alexander <alex.komyagin@10gen.com>
2013-06-20 10:40:39 -0400

    Added meaningful error messages

--- Shifkeeper/src/main/java/com/tengen/shiftkeeper/service/CrowdServiceImpl.java
+++ Shifkeeper/src/main/java/com/tengen/shiftkeeper/service/CrowdServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.tengen.shiftkeeper.common.UserObject;
@@ -29,7 +31,7 @@ public class CrowdServiceImpl implements CrowdService {
 		try {
 			UserSearchResponseObject results = (UserSearchResponseObject) restTemplate.getForObject(crowdUrl + "/search?entity-type=user&restriction={param}", UserSearchResponseObject.class,"email=" + email);
 			if (results.getUsers().size() == 0)
-				return null;
+				throw new CrowdException("No user for " + email + " found");
 			
 			for(int i=0; i< results.getUsers().size(); i++)
 			{
@@ -37,6 +39,13 @@ public class CrowdServiceImpl implements CrowdService {
 			}
 			
 			return results.getUsers().get(0);
+		} catch (HttpClientErrorException e) {
+		      switch(e.getStatusCode()) {
+		      case FORBIDDEN:
+		    	  throw new CrowdException("Failed to authenticate on Crowd");
+		      default:
+		    	  throw new CrowdException(e.getMessage());
+		      }
 		} catch(Exception e) {
 			//e.printStackTrace();
 			throw new CrowdException(e.getMessage());
@@ -48,7 +57,17 @@ public class CrowdServiceImpl implements CrowdService {
 			throws CrowdException {
 		try {
 			restTemplate.postForObject(crowdUrl + "/group/user/direct?groupname={param}", user, UserObject.class, groupName);
-			
+		} catch (HttpClientErrorException e) {
+		      switch(e.getStatusCode()) {
+		      case FORBIDDEN:
+		    	  throw new CrowdException("Failed to authenticate on Crowd");
+		      case NOT_FOUND:
+		    	  throw new CrowdException("Group " + groupName + " not found");
+		      case BAD_REQUEST:
+		    	  throw new CrowdException("User " + user.getName() + " not found");
+		      default:
+		    	  throw new CrowdException(e.getMessage());
+		      }
 		} catch(Exception e) {
 			//e.printStackTrace();
 			throw new CrowdException(e.getMessage());
@@ -60,7 +79,15 @@ public class CrowdServiceImpl implements CrowdService {
 			throws CrowdException {
 		try {
 			restTemplate.delete(crowdUrl + "/group/user/direct?groupname={param}&username={param}",groupName, user.getName());
-			
+		} catch (HttpClientErrorException e) {
+		      switch(e.getStatusCode()) {
+		      case FORBIDDEN:
+		    	  throw new CrowdException("Failed to authenticate on Crowd");
+		      case NOT_FOUND:
+		    	  throw new CrowdException("Group " + groupName + " or user " + user.getName() + " not found");
+		      default:
+		    	  throw new CrowdException(e.getMessage());
+		      }
 		} catch(Exception e) {
 			//e.printStackTrace();
 			throw new CrowdException(e.getMessage());
@@ -80,16 +107,39 @@ public class CrowdServiceImpl implements CrowdService {
 			}
 			
 			return results.getUsers();
+		} catch (HttpClientErrorException e) {
+		      switch(e.getStatusCode()) {
+		      case FORBIDDEN:
+		    	  throw new CrowdException("Failed to authenticate on Crowd");
+		      case NOT_FOUND:
+		    	  throw new CrowdException("Group " + groupName + " not found");
+		      default:
+		    	  throw new CrowdException(e.getMessage());
+		      }
 		} catch(Exception e) {
 			//e.printStackTrace();
 			throw new CrowdException(e.getMessage());
 		}
 	}
 	
-	private void fillUserInfo(UserObject user) {
-		UserObject tmp = (UserObject) restTemplate.getForObject(crowdUrl + "/user?username={param}", UserObject.class, user.getName());
-		user.setDisplayName(tmp.getDisplayName());
-		user.setEmail(tmp.getEmail());
+	private void fillUserInfo(UserObject user) throws CrowdException {
+		try {
+			UserObject tmp = (UserObject) restTemplate.getForObject(crowdUrl + "/user?username={param}", UserObject.class, user.getName());
+			user.setDisplayName(tmp.getDisplayName());
+			user.setEmail(tmp.getEmail());
+
+		} catch (HttpClientErrorException e) {
+		      switch(e.getStatusCode()) {
+		      case FORBIDDEN:
+		    	  throw new CrowdException("Failed to authenticate on Crowd");
+		      case NOT_FOUND:
+		    	  throw new CrowdException("User " + user.getName() + " not found");
+		      default:
+		    	  throw new CrowdException(e.getMessage());
+		      }
+		} catch(Exception e) {
+			throw new CrowdException(e.getMessage());
+		}
 	}
 
 }
